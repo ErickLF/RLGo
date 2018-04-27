d838cab71fa3e3d193c565afd92b0a21a0a4bbba
PatQz <patqzr@gmail.com>
2018-04-26 18:55:22 -0700

    cliff y windy con sus variantes

--- Cliff Walking/cliffwalking.go
+++ Cliff Walking/cliffwalking.go
@@ -1,5 +1,4 @@
 package main
-//----------------------------------------------------------------------
 import (
     "fmt"
     "math/rand"
@@ -14,7 +13,7 @@ const (
       worldheight int = 4 //world height
       worldwidth int = 12 //world width
       numactions int = 4 //number of actions
-  )
+)
 //----------------------------------------------------------------------
 type QLearning struct{
     Q [][]float64
@@ -30,80 +29,67 @@ type QLearning struct{
 func main() {
     rand.Seed(time.Now().Unix())
     ql:=QLearning{}
-    //states
-    ql.startX,ql.startY = 0,0
+	ql.Initialize()
+	ql.QLearning(1000)
+	ql.Print()
+}
+//----------------------------------------------------------------------
+func (ql* QLearning) Initialize(){
+	ql.startX,ql.startY = 0,0
     ql.goalX,ql.goalY = 11,0
     ql.alpha,ql.gamma,ql.epsilon=0.5,1.0,0.1
-    
-    ql.Q = make([][]float64,numactions) //4 numero de acciones
-    
-    for i:=0;i<numactions;i++{ //initialize the size of the matrix
-        ql.Q[i] = make([]float64,worldheight*worldwidth)
+    ql.Q = make([][]float64,worldheight*worldwidth) //Nuestra Q es representada con el tablero 
+	
+    for i:=0;i<worldheight*worldwidth;i++{ //initialize the size of the matrix
+        ql.Q[i] = make([]float64,numactions)
     }
-    
-    for i:=0;i<numactions;i++{  
-        for j:=0;j<worldheight*worldwidth;j++{
-            ql.Q[i][j]=rand.Float64()
+	
+    for j:=0;j<worldheight*worldwidth;j++{
+    	for i:=0;i<numactions;i++{  
+            ql.Q[j][i]=rand.Float64()
         }
-    }
-    
-    for a:=0;a<4;a++{ //we fill our matrix with zeros
-        ql.Q[a][ql.goalY*worldheight+ql.goalX] = 0
-    }
-    ql.IniciarQ()
-    ql.Print()
+    }	
 }
 //----------------------------------------------------------------------
-func (ql* QLearning) IniciarQ(){
-    episode:=1000 
+func (ql* QLearning) QLearning(episode int){
     for i:=0;i<episode;i++{//Repeart (for each step of episode )
-        //Initialize S
-        sx,sy:=ql.startX,ql.startY
-        //loop until reaches goal
-        for sx!=ql.goalX || sy!=ql.goalY{ 
-            //choose A from S using policy derived from Q
-            action:=ql.chooseAction(sx,sy)
+        sx,sy:=ql.startX,ql.startY //Initialize S
+        for sx!=ql.goalX || sy!=ql.goalY{ //loop until reaches goal
+            action:=ql.EpsilonGreedy(sx,sy)  //choose A from S using policy derived from Q
             //take action A,observe R,S' //sq -> single quote
-            R,sqsx,sqsy:=ql.move(sx,sy,action)
-            QSA:=ql.Q[action][sy*worldwidth+sx]
-            maxAction:=ql.getAction(sx,sy)
-            sqQSA:=ql.Q[maxAction][sqsy*worldwidth+sqsx]
+            R,sqsx,sqsy:=ql.Move(sx,sy,action)
+            QSA:=ql.Q[GetGridPos(sx,sy)][action]
+            maxAction:=ql.GetAction(sx,sy)
+            sqQSA:=ql.Q[GetGridPos(sqsx,sqsy)][maxAction]
             //Q(S,A)<-Q(S,A)+alpha*[R+gammaMax v(a) Q(S',a)-Q(S,A)]
             Q:=QSA+ql.alpha*(R+ql.gamma*(sqQSA-QSA))
-            ql.Q[action][sy*worldwidth+sx]=Q
-            //S<-S'
-            sx=sqsx
-            sy=sqsy
-            //until S is terminal
+			ql.Q[GetGridPos(sx,sy)][action]=Q
+            //S<-S' //until S is terminal
+            sx,sy=sqsx,sqsy
         }
     }
 }
 //----------------------------------------------------------------------
-func (q *QLearning) chooseAction(x,y int) int{ //epsilon greedy
-	if rand.Float64() < 1-q.epsilon {
-		return q.getAction(x, y)
-	}
-  
+func (q *QLearning) EpsilonGreedy(x,y int) int{ //epsilon greedy
+	if rand.Float64() < 1-q.epsilon { return q.GetAction(x, y) }
 	return rand.Intn(numactions)
 }
 //----------------------------------------------------------------------
-func (ql* QLearning) getAction(x,y int) int {//both parameters are int and returns int
-  ids := y*worldwidth+x //id of square
-	max := ql.Q[0][ids]
+func (ql* QLearning) GetAction(x,y int) int {//both parameters are int and returns int
+	gridPos := GetGridPos(x,y) //position in grid
+	max := ql.Q[gridPos][0] //taking the first action of position in grid
 	action := 0
 	for i := 0; i < numactions; i++ {
-		if max < ql.Q[i][ids] {
-			max = ql.Q[i][ids]
+		if max < ql.Q[gridPos][i] {
+			max = ql.Q[gridPos][i]
 			action = i
 		}
 	}
-  
 	return action
 }
 //----------------------------------------------------------------------
-func (ql* QLearning) move(x,y,action int) (float64,int,int){
+func (ql* QLearning) Move(x,y,action int) (float64,int,int){
   qx,qy:=x,y
-  
   switch action{
     case up:
       if y!=worldheight-1{ qy=y+1 }
@@ -114,34 +100,36 @@ func (ql* QLearning) move(x,y,action int) (float64,int,int){
     case right:
       if x!=worldwidth-1{ qx=x+1 }
   }
-  
-  if qy == 0 && qx >= 1 && qx < worldwidth-1 { //if it falls in the cliff
+  if qy == 0 && qx >= 1 && qx < worldwidth-1 { //If falls in the cliff
 		return -100.0, 0, 0
   }else if qy==ql.goalY && qx==ql.goalX{ //If reaches goal
-		return 0.0, ql.goalX, ql.goalY
-	}
-  
+		return 0.0, ql.goalX, ql.goalY 
+  }
+	
   return -1.0,qx,qy//if doesnt reach goal OR doesnt fall on THE CLIFF just continue on board
 }
 //----------------------------------------------------------------------
-/**Funcion super bonis hecha originalmente por Github:@JoseGurrola**/
+func GetGridPos(x,y int) int{
+	return y * worldwidth + x
+}
+//----------------------------------------------------------------------
 func (q *QLearning) Print() {
 	for i := worldheight - 1; i >= 0; i-- {
 		for j := 0; j < worldwidth; j++ {
 			if i != q.goalY || j != q.goalX {
 				if j > 0 && j < worldwidth-1 && i == 0 {
-					fmt.Print("<- ")
+					fmt.Print("←  ")
 				} else {
-					switch q.getAction(j, i) {
-					case up:
-						fmt.Print("U  ")
-					case down:
-						fmt.Print("D  ")
-					case left:
-						fmt.Print("L  ")
-					case right:
-						fmt.Print("R  ")
-					}
+					switch q.GetAction(j, i) {
+						case up:
+							fmt.Print("↑  ")
+						case down:
+							fmt.Print("↓  ")
+						case left:
+							fmt.Print("←  ")
+						case right:
+							fmt.Print("→  ")
+						}
 				}
 			} else {
 				fmt.Print("G ")
---
+++ Windy Gridworld/stochastickingwindygridworld.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
+
+const(
+	up int = iota
+	down
+	left
+	right
+    up_left
+    up_right
+    down_left
+    down_right
+	numactions int = 8
+	worldheight=7
+	worldwidth=10
+	)
+//----------------------------------------------------------------------
+type Sarsa struct{
+	Q[][]float64
+	startX int
+	startY int
+	goalX int
+	goalY int
+	epsilon float64
+	alpha float64
+	gamma float64
+	wind [] int
+}
+//----------------------------------------------------------------------
+func main(){
+	rand.Seed(time.Now().Unix())
+	fmt.Println(" STOCHASTIC KING WINDY GRID WORLD")
+	sarsa:=Sarsa{}
+	sarsa.InitSarsa()
+	sarsa.Sarsas(2000)
+	sarsa.Print()
+}
+//----------------------------------------------------------------------
+func(q *Sarsa) InitSarsa(){
+	//Initialize Q(s,a),for all s belonging to S, a belonging to A(s), arbitrarily, and Q(terminal-state,.)=0
+	q.startX,q.startY = 0,3
+	q.goalX,q.goalY = 7,3
+	q.epsilon,q.gamma,q.alpha=0.1,1.0,0.5
+	q.wind = []int{0, 0, 0, 1, 1, 1, 2, 2, 1, 0}
+	q.Q = make([][]float64,worldheight*worldwidth)
+
+	for i:=0;i<worldheight*worldwidth;i++{ //initialize the size of the matrix
+        q.Q[i] = make([]float64,numactions)
+    }
+	
+    for j:=0;j<worldheight*worldwidth;j++{
+    	for i:=0;i<numactions;i++{  
+            q.Q[j][i]=rand.Float64()
+        }
+    }	
+}
+//----------------------------------------------------------------------
+func (s*Sarsa)Sarsas(episodes int){
+	for i:=0;i<episodes;i++{ //Repeat (for each episode)
+		sx,sy:=s.startX,s.startY //Initialize S
+		action:=s.EpsilonGreedy(sx,sy) //Choose A from S using policy derived from Q(e.g.,epsilon-Greedy)
+		for sx!=s.goalX || sy!=s.goalY{ //Repeat (for each step of the episode): that is, until you reach the goal
+      		reward,sq_sx,sq_sy:=s.Move(sx,sy,action)//Take action A, observe R, S'//sq -> single quote	
+			sq_action:=s.EpsilonGreedy(sq_sx,sq_sy)//Choose  A' from S' using policy derived from Q (e.g.,epsisol-Greedy)
+			//Q(S,A) <- Q(S,A) + alpha[R+gammaQ(S',A')-Q(S,A)]
+			QSA:=s.Q[GetGridPos(sx,sy)][action]
+			sqQSA:=s.Q[GetGridPos(sq_sx,sq_sy)][sq_action]
+			Q:= QSA + s.alpha*(reward+s.gamma*sqQSA-QSA)
+			s.Q[GetGridPos(sx,sy)][action] = Q
+			//S <- S'; A <- A'//until S is terminal
+			sx,sy=sq_sx,sq_sy
+			action=sq_action
+		}
+	}
+}
+//----------------------------------------------------------------------
+func (q *Sarsa) EpsilonGreedy(x,y int) int{ //epsilon greedy
+	if rand.Float64() < 1-q.epsilon { return q.GetAction(x, y) }
+	return rand.Intn(numactions)
+}
+//----------------------------------------------------------------------
+func (ql* Sarsa) GetAction(x,y int) int {//both parameters are int and returns int
+	gridPos := GetGridPos(x,y) //position in grid
+	max := ql.Q[gridPos][0] //taking the first action of position in grid
+	action := 0
+	for i := 0; i < numactions; i++ {
+		if max < ql.Q[gridPos][i] {
+			max = ql.Q[gridPos][i]
+			action = i
+		}
+	}
+	return action
+}
+//----------------------------------------------------------------------
+func (ql* Sarsa) Move(x,y,action int) (float64,int,int){
+  qx,qy:=x,y
+  switch action{
+      case up:
+        if y!=worldheight-1{ qy=y+1 }
+      case down:
+        if y!=0{ qy=y-1 }
+      case left:
+        if x!=0{ qx=x-1 }
+      case right:
+        if x!=worldwidth-1{ qx=x+1 }
+      case up_left:
+        if x!=0{ qx = x-1 }
+        if y!=worldheight-1{ qy=y+1 }
+      case up_right:
+       if x != worldwidth-1 { qx = x + 1 }
+       if y != worldheight-1 { qy = y + 1 }
+      case down_left:
+       if x != 0 { qx = x - 1 }
+       if y != 0 { qy = y - 1 }
+      case down_right:
+       if x != worldwidth-1 { qx = x + 1 }
+       if y != 0 { qy = y - 1 }
+   }
+    qy+= ql.wind[x]
+    rana:=random.float64
+    if rana < (1.0/3.0){
+        qy+=1
+    }elif rana < (2.0/3.0){
+        qy-=1
+    }else{
+        qy-=ql.wind[x]
+    }
+    
+   if qy > worldheight-1{ qy=worldheight-1 }  
+    
+   if qy==ql.goalY && qx==ql.goalX{ //If reaches goal
+		return 0.0, ql.goalX, ql.goalY 
+  }
+	
+  return -1.0,qx,qy//if doesnt reach goal OR doesnt fall on THE CLIFF just continue on board
+}
+//----------------------------------------------------------------------
+func GetGridPos(x,y int) int{
+	return y * worldwidth + x
+}
+//----------------------------------------------------------------------
+func (q *Sarsa) Print() {
+	for i := worldheight - 1; i >= 0; i-- {
+		for j := 0; j < worldwidth; j++ {
+			if i != q.goalY || j != q.goalX {
+					switch q.GetAction(j, i) {
+						case up:
+							fmt.Print("↑  ")
+						case down:
+							fmt.Print("↓  ")
+						case left:
+							fmt.Print("←  ")
+						case right:
+							fmt.Print("→  ")
+                        case up_left:
+                            fmt.Print("↖  ")
+                        case up_right:
+                            fmt.Print("↗  ")
+                        case down_left:
+                            fmt.Print("↙  ")
+                        case down_right:
+                            fmt.Print("↘  ")
+						}
+			} else {
+				fmt.Print("G  ")
+			}
+		}
+		fmt.Println("")
+	}
+    
+    for i:= 0; i<len(q.wind); i++{
+        fmt.Print(q.wind[i],"  ")
+    }
+    fmt.Println("= wind")
+}
